cbbed35aa91d2fd36b588a40b966c745b7aa877b
ManuelRRo <ds23906@gmail.com>
2023-10-16 23:10:36 -0600

    Agregar Pantalla de Registro y validaciones v1

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -3,10 +3,16 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegisterUserForm(UserCreationForm):
-	email = forms.EmailField()
-	first_name = forms.CharField(max_length=50)
-	last_name = forms.CharField(max_length=50)
-
+	password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control form-control-user','placeholder':'Contraseña'}),
+    )
+	password2 = forms.CharField(
+		widget=forms.PasswordInput(attrs={'class': 'form-control form-control-user','placeholder':'Confirmar Contraseña'}),
+	)
 	class Meta:
 		model = User
-		fields = ('username','first_name','last_name','email','password1','password2')
+		fields = ('username','email','password1','password2')
+		widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control form-control-user','placeholder':'Nombre de Usuario'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control form-control-user','placeholder':'Correo'}),
+        }
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import RegisterUserForm
+from contabilidad.models import Propietario
 
 def login_user(request):
     if request.method == "POST":
@@ -17,3 +18,24 @@ def login_user(request):
 
     else:
         return render(request,'registration/login.html')
+
+def register_user(request):
+    context = {}
+    if request.method == "POST":
+        register_form = RegisterUserForm(request.POST)
+        if register_form.is_valid():
+            user = register_form.save()
+            Propietario.objects.create(user=user)
+            return redirect('login')
+        else:
+            context['form'] = register_form
+            return render(request,'registration/sign_up.html',context)
+            
+    else:
+        context['form'] = RegisterUserForm()
+
+    return render(request,'registration/sign_up.html',context)
+
+        
+
+
